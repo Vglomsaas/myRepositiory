cd22c5bb45335cf82a758c1ac7c0fcda97b4f152
Vetle G. Kalsaas <vetle.k@hotmail.no>
2023-06-10 12:49:09 +0200

    Worked on SetupQuiz class

--- src/main/java/SetupQuiz.java
+++ src/main/java/SetupQuiz.java
@@ -1,11 +1,24 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class SetupQuiz {
-    HashMap<Integer, String> binaryQuestions = new HashMap<>();
-    HashMap<Integer, String> multichoiceQuestions = new HashMap<>();
+    static {
+        database = new Database("quizDB", "user", "password");
+    }
+    static Database database;
+    private HashMap<Integer, String> binaryQuestions = new HashMap<>();
+    private HashMap<Integer, String> multichoiceQuestions = new HashMap<>();
 
+    try (Connection connection = database.getConnection()) {
+        String query = "SELECT * FROM binaryQuiz";
 
+        Statement statement = connection.createStatement();
 
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
 }
